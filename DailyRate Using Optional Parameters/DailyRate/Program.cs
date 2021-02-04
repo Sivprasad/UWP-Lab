b2dc2bd0c664db68b7653ee7ec5f67be9fc5fc30
Sivprasad <sivaprasad.g@live.com>
2021-02-04 20:39:09 +0530

    Completed first phase

---
+++ DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            //double fee = calculateFee();
+            //double fee = calculateFee(650.0);
+            // double fee = calculateFee(500.0,3);
+           double fee = calculateFee(theDailyrate: 375.0);
+            Console.WriteLine($"Fee is {fee}");
+        }
+        private double calculateFee(double theDailyrate = 500.0, int noOfDays =1)
+        {
+            Console.WriteLine("CalculateFee using two optional parameters: ");
+            return theDailyrate * noOfDays;
+        }
+        private double calculateFee(double dailyrate = 500.0 )
+        {
+            Console.WriteLine("CalculateFee using one optional parameters: ");
+            int noOfDays = 1;
+            return dailyrate * noOfDays;
+        }
+
+        private double calculateFee()
+        {
+            Console.WriteLine("CalculateFee using  hardcoded values ");
+            double dailyrate = 400.0;
+            int noOfDays = 1;
+            return dailyrate * noOfDays;
+        }
+    }
+}
---
+++ DailyRate/DailyRate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(CalculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double v) => Console.WriteLine($"The calculation Fees is {v * 1.1} ");
+        
+
+        private double CalculateFee(double dailyRate, int noOfDays) =>  dailyRate* noOfDays;
+        private int readInt(string v) 
+        {
+            Console.Write(v);
+            int noOfDays = int.Parse(Console.ReadLine());
+            return noOfDays;
+        }
+
+        private double readDouble(string v)
+        {
+            Console.WriteLine(v);
+            double dailyRate = double.Parse(Console.ReadLine());
+            return dailyRate;
+        }
+    }
+}
